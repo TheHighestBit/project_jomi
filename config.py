a434fdd7d6d201273bde904525e206cd3c6dae78
Fent <jooseporasmae@hotmail.com>
2021-10-20 19:37:59 +0300

    Defineerisin registrid config.py-s

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
+from registers import Register
+
 #Siia tuleks defineerida globaalsed muutujad, mida üle kogu projekti kasutada
 #Nt. tuleks siin defineerida kõik registrid, mälu olek jne.
+
+r0 = '0b000000000000000' #Selle registri väärtus on alati null!
+r1 = Register()
+r2 = Register()
+r3 = Register()
+r4 = Register()
+r5 = Register()
+r6 = Register()
+r7 = Register()
--- instructions.py
+++ instructions.py
@@ -1,4 +1,4 @@
 #Siia tuleb käskude loogika, kokku 8 käsku
 
 def ADD(rA, rB, rC): #Add contents of regB with regC, store result in regA
-    rA.store(bin(rB.value() + rC.value()))
+    rA.store(rB.value() + rC.value())
