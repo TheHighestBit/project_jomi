9ac4a4cfb8e7368503a925b8a6dff09b954770ad
JaanKupri <kuprijaan@gmail.com>
2021-12-10 16:58:49 +0200

    GUI designeriga

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,15 +5,23 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore, uic
 from PyQt5.QtGui import QCursor
 
+
 class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi("basic.ui", self)
+        uic.loadUi("MainWindow.ui", self)
+        self.button2 = QPushButton("Automaatne")
+        def OtherWindow():
+            uic.loadUi("OtherWindow.ui",self)
+        self.button2.clicked.connect(OtherWindow)
         self.show()
+    
         
+   
+    
         
 
-
+        
 app = QApplication(sys.argv)
 window = Ui()
 app.exec_()
